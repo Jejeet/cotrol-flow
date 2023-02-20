a3e6b21658124153c568b383cba67aa44a32eccd
Adewumi <owonikokoadewunmi@gmail.com>
2023-02-20 12:38:05 +0100

    if else

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-for (let i=1; i < 20; i +=7){
-    console.log(i);
+//3
+let scienceSubjects = "Physics,Chemistry,Biology, Technical Drawing"
+let socialScienceSubjects ="Accounting, Commerce,Marketing,Geography"
+let artsSubjects ="Government, Economics,Literature, History"
+let generalSubjects =" English ,Mathematics"
+
+let studyCourse =artsSubjects
+
+if (studyCourse == artsSubjects){
+    console.log(artsSubjects + generalSubjects)
 }
+else if(studyCourse == scienceSubjects){
+    console.log(scienceSubjects + generalSubjects)
+} 
+else if(studyCourse == socialScienceSubjects){
+    console.log(socialScienceSubjects + generalSubjects)
+} 
+else{console.log(generalSubjects)}
